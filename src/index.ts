416e67104ac4a66e53476751e3e4058f81d1126c
erichodges <14981329+erichodges@users.noreply.github.com>
2019-08-24 15:10:32 -0700

    Build config: moved const port in index.ts

--- src/index.ts
+++ src/index.ts
@@ -28,7 +28,6 @@ const startServer = async () => {
         console.log("DB connection error:" + e.message)
       ); // connects to the DB
 
-  const port = process.env.PORT || 4000;
   const app = express();
 
   app.use(
@@ -46,6 +45,8 @@ const startServer = async () => {
   );
 
   server.applyMiddleware({ app, cors: false }); // app is from an existing express app
+
+  const port = process.env.PORT || 4000;
 
   app.listen({ port }, () =>
     console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
